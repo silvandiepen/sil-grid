c36b5e653fdbd6dea272f4a7a607686de671c76f
Sil van Diepen <me@silvandiepen.com>
2018-10-10 11:32:36 +0200

    initial commit

---
+++ src/column.vue
@@ -0,0 +1,27 @@
+<template>
+	<div :class="classNames">
+		<slot></slot>
+	</div>
+</template>
+
+<script>
+import gridMethods from './methods.js';
+
+export default{
+  name: 'column',
+	props: ['small','medium','large','xlarge','xxlarge','background-color','text-color'],
+	data() {
+		return{
+			classNames: ['column'],
+		}
+	},
+	methods: gridMethods.methods,
+	created() {
+		if(this.$props.small == undefined){
+			this.classNames.push('small-full');
+		}
+		this.classNames.push(this.setClasses(this.$props,this));
+		this.classNames.push(this.setWidth(this.$props,this));
+	}
+}
+</script>
---
+++ src/index.js
@@ -0,0 +1,34 @@
+// Import vue component
+import row from './row.vue';
+import column from './column.vue';
+
+// install function executed by Vue.use()
+export function install(Vue) {
+	if (install.installed) return;
+	install.installed = true;
+	Vue.component('Row', row);
+	Vue.component('Column', column);
+}
+
+// Create module definition for Vue.use()
+const plugin = {
+	install,
+};
+
+// To auto-install when vue is found
+let GlobalVue = null;
+if (typeof window !== 'undefined') {
+	GlobalVue = window.Vue;
+} else if (typeof global !== 'undefined') {
+	GlobalVue = global.Vue;
+}
+if (GlobalVue) {
+	GlobalVue.use(plugin);
+}
+
+// To allow use as module (npm/webpack/etc.) export component
+export default { row, column };
+
+// It's possible to expose named exports when writing components that can
+// also be used as directives, etc. - eg. import { RollupDemoDirective } from 'rollup-demo';
+// export const RollupDemoDirective = component;
