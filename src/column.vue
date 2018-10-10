98035cb6e4041eef4111280eda9da37a73531762
Sil van Diepen <me@silvandiepen.com>
2018-10-10 11:50:40 +0200

    cleanup props

--- src/column.vue
+++ src/column.vue
@@ -7,21 +7,50 @@
 <script>
 import gridMethods from './methods.js';
 
-export default{
-  name: 'column',
-	props: ['small','medium','large','xlarge','xxlarge','background-color','text-color'],
-	data() {
-		return{
-			classNames: ['column'],
+export default {
+	name: 'column',
+	props: {
+		small: {
+			type: String,
+			default: null
+		},
+		medium: {
+			type: String,
+			default: null
+		},
+		large: {
+			type: String,
+			default: null
+		},
+		xlarge: {
+			type: String,
+			default: null
+		},
+		xxlarge: {
+			type: String,
+			default: null
+		},
+		backgroundColor: {
+			type: String,
+			default: null
+		},
+		textColor: {
+			type: String,
+			default: null
 		}
+	},
+	data() {
+		return {
+			classNames: ['column']
+		};
 	},
 	methods: gridMethods.methods,
 	created() {
-		if(this.$props.small == undefined){
+		if (this.$props.small == undefined) {
 			this.classNames.push('small-full');
 		}
-		this.classNames.push(this.setClasses(this.$props,this));
-		this.classNames.push(this.setWidth(this.$props,this));
+		this.classNames.push(this.setClasses(this.$props, this));
+		this.classNames.push(this.setWidth(this.$props, this));
 	}
-}
+};
 </script>
--- src/row.vue
+++ src/row.vue
@@ -1,24 +1,37 @@
 <template>
-	<div :class="classNames">
-		<slot></slot>
-	</div>
+  <div :class="classNames">
+    <slot></slot>
+  </div>
 </template>
 
 <script>
-import gridMethods from "./methods.js";
+import gridMethods from './methods.js';
 
 export default {
-  name: "row",
-  props: ["center", "background-color", "text-color"],
-  data() {
-    return {
-      classNames: ["row"]
-    };
-  },
-  methods: gridMethods.methods,
-  created() {
-    this.classNames.push(this.setClasses(this.$props, this));
-    this.classNames.push(this.setWidth(this.$props, this));
-  }
+	name: 'row',
+	props: {
+		center: {
+			type: String,
+			default: null
+		},
+		backgroundColor: {
+			type: String,
+			default: null
+		},
+		textColor: {
+			type: String,
+			default: null
+		}
+	},
+	data() {
+		return {
+			classNames: ['row']
+		};
+	},
+	methods: gridMethods.methods,
+	created() {
+		this.classNames.push(this.setClasses(this.$props, this));
+		this.classNames.push(this.setWidth(this.$props, this));
+	}
 };
 </script>
